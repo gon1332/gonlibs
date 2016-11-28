3fe8f0910f299e16482d216428f2fe135dfd332e
Ioannis Konstadelias <ikonstadel@gmail.com>
2016-11-29 01:21:56 +0200

    Add levels for logging.

--- include/DBG/logger.h
+++ include/DBG/logger.h
@@ -5,7 +5,20 @@
 #include <string.h>
 #include <errno.h>
 
-#define LOG(msg, ...)   fprintf(stderr, msg, ##__VA_ARGS__)
+#define LOG_LEVEL   1
+#define LVL_HIGH    3
+#define LVL_MEDIUM  2
+#define LVL_LOW     1
+#define LVL_NO      0
+#define LOG(lvl, msg, ...) \
+    do { \
+        switch (LEVEL) { \
+            case LVL_NO: break; \
+            case LVL_LOW:    (void)((lvl >= LVL_LOW)    && fprintf(stderr, msg, ##__VA_ARGS__)); break; \
+            case LVL_MEDIUM: (void)((lvl >= LVL_MEDIUM) && fprintf(stderr, msg, ##__VA_ARGS__)); break; \
+            case LVL_HIGH:   (void)((lvl >= LVL_HIGH)   && fprintf(stderr, msg, ##__VA_ARGS__)); break; \
+        } \
+    } while (0)
 
 
 #define ERRNO_REPORT_EXIT(reason, file, line) \
--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "tests.h"
 #include "DBG/logger.h"
 
+#undef  LEVEL
+#define LEVEL 0
+
 int main(void)
 {
-    LOG("start of testing\n");
+    LOG(1, "%s", "start of testing\n");
     CHECK_SANITY(test__MEM_memwrap(),   "MEM_memwrap");
     CHECK_SANITY(test__ADT_stack(),     "ADT_stack");
     CHECK_SANITY(test__ADT_dequeue(),   "ADT_dequeue");
     CHECK_SANITY(test__ADT_hashtable(), "ADT_hashtable");
-    LOG("end of testing\n");
+    LOG(2, "%s", "end of testing\n");
 
     return EXIT_SUCCESS;
 }
